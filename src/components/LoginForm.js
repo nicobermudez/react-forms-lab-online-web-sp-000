a81eeb6adc648b2bd58404097bacd6ce05bf7b65
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-26 16:08:02 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -11,7 +11,7 @@ class LoginForm extends React.Component {
   }
 
   handleChange = event => {
-    this.setState({
+    this.setState = ({
       [event.target.name]: event.target.value
     })
   }
